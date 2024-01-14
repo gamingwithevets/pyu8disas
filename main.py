4f420fd057260e3cd49a8f02f425755142165440
Steveyboi <thenewgwe@gmail.com>
2024-01-14 18:04:09 +0700

    Fix bugs

--- main.py
+++ main.py
@@ -372,7 +372,8 @@ class Disasm:
 			radr = self.start + self.addr + 2 + skip
 			radr_ = radr - self.start
 			if only_labels:
-				if radr_ in self.labels: ins_str = ins_str.replace('#Radr', self.labels[radr_][0])
+				label = self.labels[radr_]
+				if radr_ in self.labels: ins_str = ins_str.replace('#Radr', self.labels[label[2]][0] + label[0])
 				else: ins_str = ins_str.replace('#Radr', self.fmt_addr(radr))
 			else:
 				if radr > 5 and self.start <= radr < self.start + len(self.input_file):
